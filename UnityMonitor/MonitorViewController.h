53e0160d41125ddb3a730444205d999595abe843
lukasmusy <lukasmusy@gmail.com>
2016-11-12 17:09:41 +0100

    -  tabelviewcontroller impl

--- UnityMonitor/MonitorViewController.h
+++ UnityMonitor/MonitorViewController.h
@@ -11,29 +11,13 @@
 
 @class OscHandler;
 
-@interface MonitorViewController : UIViewController
+@interface MonitorViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) PDPatch *patch;
 @property (strong, nonatomic) OscHandler *oschandler;
 
+@property (nonatomic, strong) NSArray *monitorData;
 
-@property (weak, nonatomic) IBOutlet UILabel *parameter1_label;
-@property (weak, nonatomic) IBOutlet UILabel *parameter2_label;
-@property (weak, nonatomic) IBOutlet UILabel *parameter3_label;
-@property (weak, nonatomic) IBOutlet UILabel *parameter4_label;
-@property (weak, nonatomic) IBOutlet UILabel *parameter5_label;
-@property (weak, nonatomic) IBOutlet UILabel *parameter6_label;
-@property (weak, nonatomic) IBOutlet UILabel *parameter7_label;
-@property (weak, nonatomic) IBOutlet UILabel *parameter8_label;
-
-@property (weak, nonatomic) IBOutlet UILabel *value1_label;
-@property (weak, nonatomic) IBOutlet UILabel *value2_label;
-@property (weak, nonatomic) IBOutlet UILabel *value3_label;
-@property (weak, nonatomic) IBOutlet UILabel *value4_label;
-@property (weak, nonatomic) IBOutlet UILabel *value5_label;
-@property (weak, nonatomic) IBOutlet UILabel *value6_label;
-@property (weak, nonatomic) IBOutlet UILabel *value7_label;
-@property (weak, nonatomic) IBOutlet UILabel *value8_label;
 
 - (void)displayParam: (NSString *)parameter withValue: (NSString *)value inSlot:(int)slot;
 
--- UnityMonitor/MonitorViewController.m
+++ UnityMonitor/MonitorViewController.m
@@ -20,106 +20,113 @@
 
 @implementation MonitorViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.patch = [[PDPatch alloc] initWithFile:@"main.pd"];
     self.oschandler = [[OscHandler alloc]initWithOwner:self];
 
-
 }
 
-- (void)initLabels {
-    [self.parameter1_label setTextColor:[UIColor blueColor]];
-    [self.parameter1_label setHighlightedTextColor:[UIColor redColor]];
-    [self.value1_label setTextColor:[UIColor blueColor]];
-    [self.value1_label setHighlightedTextColor:[UIColor redColor]];
-
-    [self.parameter1_label setTextColor:[UIColor blueColor]];
-    [self.parameter1_label setHighlightedTextColor:[UIColor redColor]];
-    [self.value1_label setTextColor:[UIColor blueColor]];
-    [self.value1_label setHighlightedTextColor:[UIColor redColor]];
+- (void)loadView
+{
+    UITableView *tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame] style:UITableViewStylePlain];
+    tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    [tableView reloadData];
     
-    [self.parameter1_label setTextColor:[UIColor blueColor]];
-    [self.parameter1_label setHighlightedTextColor:[UIColor redColor]];
-    [self.value1_label setTextColor:[UIColor blueColor]];
-    [self.value1_label setHighlightedTextColor:[UIColor redColor]];
-    
-    [self.parameter1_label setTextColor:[UIColor blueColor]];
-    [self.parameter1_label setHighlightedTextColor:[UIColor redColor]];
-    [self.value1_label setTextColor:[UIColor blueColor]];
-    [self.value1_label setHighlightedTextColor:[UIColor redColor]];
-    
+    self.view = tableView;
 }
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *watcher = @"Watcher";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:watcher];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:watcher];
+    }
+    
+    cell.textLabel.text = @"test";
+    
+    return cell;
+}
 
 - (void)displayParam: (NSString *)parameter withValue: (NSString *)value inSlot:(int)slot{
     
-    switch (slot) {
-        case 1: {
-
-            [self.parameter1_label setText:parameter];
-            [self.value1_label setText:value];
-            [self.value1_label setHighlighted:YES];
-            break;
-        }
-            
-        case 2: {
-            [self.parameter2_label setTextColor:[UIColor blueColor]];
-            [self.value2_label setTextColor:[UIColor blueColor]];
-            [self.parameter2_label setText:parameter];
-            [self.value2_label setText:value];
-            break;
-        }
-            
-        case 3: {
-            [self.parameter3_label setTextColor:[UIColor blueColor]];
-            [self.value3_label setTextColor:[UIColor blueColor]];
-            [self.parameter3_label setText:parameter];
-            [self.value3_label setText:value];
-            break;
-        }
-            
-        case 4: {
-            [self.parameter4_label setTextColor:[UIColor blueColor]];
-            [self.value4_label setTextColor:[UIColor blueColor]];
-            [self.parameter4_label setText:parameter];
-            [self.value4_label setText:value];
-            break;
-        }
-            
-        case 5: {
-            [self.parameter5_label setTextColor:[UIColor blueColor]];
-            [self.value5_label setTextColor:[UIColor blueColor]];
-            [self.parameter5_label setText:parameter];
-            [self.value5_label setText:value];
-            break;
-        }
-            
-        case 6: {
-            [self.parameter6_label setTextColor:[UIColor blueColor]];
-            [self.value6_label setTextColor:[UIColor blueColor]];
-            [self.parameter6_label setText:parameter];
-            [self.value6_label setText:value];
-            break;
-        }
-            
-        case 7: {
-            [self.parameter7_label setTextColor:[UIColor blueColor]];
-            [self.value7_label setTextColor:[UIColor blueColor]];
-            [self.parameter7_label setText:parameter];
-            [self.value7_label setText:value];
-            break;
-        }
-            
-        case 8: {
-            [self.parameter8_label setTextColor:[UIColor blueColor]];
-            [self.value8_label setTextColor:[UIColor blueColor]];
-            [self.parameter8_label setText:parameter];
-            [self.value8_label setText:value];
-            break;
-        }
-    }
+//    switch (slot) {
+//        case 1: {
+//
+//            [self.parameter1_label setText:parameter];
+//            [self.value1_label setText:value];
+//            [self.value1_label setHighlighted:YES];
+//            break;
+//        }
+//            
+//        case 2: {
+//            [self.parameter2_label setTextColor:[UIColor blueColor]];
+//            [self.value2_label setTextColor:[UIColor blueColor]];
+//            [self.parameter2_label setText:parameter];
+//            [self.value2_label setText:value];
+//            break;
+//        }
+//            
+//        case 3: {
+//            [self.parameter3_label setTextColor:[UIColor blueColor]];
+//            [self.value3_label setTextColor:[UIColor blueColor]];
+//            [self.parameter3_label setText:parameter];
+//            [self.value3_label setText:value];
+//            break;
+//        }
+//            
+//        case 4: {
+//            [self.parameter4_label setTextColor:[UIColor blueColor]];
+//            [self.value4_label setTextColor:[UIColor blueColor]];
+//            [self.parameter4_label setText:parameter];
+//            [self.value4_label setText:value];
+//            break;
+//        }
+//            
+//        case 5: {
+//            [self.parameter5_label setTextColor:[UIColor blueColor]];
+//            [self.value5_label setTextColor:[UIColor blueColor]];
+//            [self.parameter5_label setText:parameter];
+//            [self.value5_label setText:value];
+//            break;
+//        }
+//            
+//        case 6: {
+//            [self.parameter6_label setTextColor:[UIColor blueColor]];
+//            [self.value6_label setTextColor:[UIColor blueColor]];
+//            [self.parameter6_label setText:parameter];
+//            [self.value6_label setText:value];
+//            break;
+//        }
+//            
+//        case 7: {
+//            [self.parameter7_label setTextColor:[UIColor blueColor]];
+//            [self.value7_label setTextColor:[UIColor blueColor]];
+//            [self.parameter7_label setText:parameter];
+//            [self.value7_label setText:value];
+//            break;
+//        }
+//            
+//        case 8: {
+//            [self.parameter8_label setTextColor:[UIColor blueColor]];
+//            [self.value8_label setTextColor:[UIColor blueColor]];
+//            [self.parameter8_label setText:parameter];
+//            [self.value8_label setText:value];
+//            break;
+//        }
+//    }
 }
 
 - (void)soundEnable:(BOOL)b {
